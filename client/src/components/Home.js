e30cd3ae5f4b125d55786be48dd78ec66c6fde55
manish <manish@antarx.tech>
2019-12-19 15:32:38 +0530

    initial setup

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,77 +1,50 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {handleInputAction, fetchWishAction, handleSubmitAction, handleDeleteAction} from '../myaction/action';
 
 class Home extends React.Component{
-  state={
-    text:'',
-    mywishes:[{_id:1,wish:"loading"}]
-  }
-  handledelete(id){
-    fetch('/remove/'+id,{method:"delete"})
-    .then(res=>res.json())
-    .then(res2=>{
-      console.log(res2)
-      const newWishes = this.state.mywishes.filter(item=>{
-        return item._id !== res2._id
-      })
-      this.setState({
-        mywishes:newWishes
-      })
-    })
-  }
-  componentDidMount(){
-    fetch('/data')
-    .then(res=>res.json())
-    .then(res2=>{
-      console.log(res2)
-      this.setState({
-        mywishes:res2
-      })
-    })
-  }
-  handleSubmit(e){
-        e.preventDefault();
-       // const url = "http://localhost:5000/sent";
-        var data = new URLSearchParams();
-         for(const pair of new FormData(e.target)){
-           data.append(pair[0],pair[1])
-         }
-         //localhost:5000/sent
-        fetch('/sent',{
-            method:"post",
-            body:data,
-           
-        }).then(res=>res.json())
-        .then(res2 => {
-          console.log(res2)
-        
-          this.setState({
-            mywishes:[...this.state.mywishes,res2]
-          })
-        }); 
-  }
-  render(){
-     const list = this.state.mywishes.map(item=>{
-       return <a className="collection-item" key={item._id} onClick={()=>this.handledelete(item._id)}>{item.wish}</a>
-     })
-      return(
-        <div>
-          <form onSubmit={(e)=>this.handleSubmit(e)}>
-            <input 
-              type="text"
-              name="item"
-              value={this.state.text} 
-              onChange={(e)=>this.setState({text:e.target.value})}
-               />
-              <button type="submit" className="waves-effect waves-light btn">Add</button> 
-          </form>
+	componentDidMount(){
+		console.log(' i am in the como did mount', this.props);
+		this.props.fetchwish()
+	}
 
-       <div className="collection">
-         {list}
-      </div>
-        </div>
-      )
-  }
- 
+	render(){
+		const list = this.props.mywishes.map(item=>{
+			return <a className="collection-item" key={item._id} onClick={()=>this.props.handleDelete(item._id)}>{item.wish}</a>
+				})
+		return(
+			<div>
+				<form onSubmit={(e)=>this.props.handleSubmit(e)}>
+					<input 
+						type="text"
+						name="item"
+						value={this.props.text} 
+						onChange={(e)=>this.props.handleInput(e.target.value)}
+						/>
+						<button type="submit" className="waves-effect waves-light btn">Add</button> 
+					</form>
+				<div className="collection">
+				{list}
+			</div>
+		</div>
+	)
+	}
 }
 
-export default Home;
+const mapStateToProps = (state)=>{
+	return {
+		text     : state.text,
+		mywishes : state.mywishes
+	}
+}
+
+const mapDispatchToProps = (dispatch)=>{
+	return {
+		handleInput  : (input)=>{dispatch(handleInputAction(input))},
+		fetchwish    : ()=>{dispatch(fetchWishAction())},
+		handleSubmit : (e)=>{dispatch(handleSubmitAction(e))},
+		handleDelete : (id)=>{dispatch(handleDeleteAction(id))}
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
